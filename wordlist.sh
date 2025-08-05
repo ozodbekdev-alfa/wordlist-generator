dfa86518fff1276bb8179ce3341974c05217b00b
ozodbekdev-alfa <ozodbek04052008@gmail.com>
2025-08-05 10:20:41 +0500

    Create wordlist.sh

---
+++ wordlist.sh
@@ -0,0 +1,11 @@
+clear
+echo "
+
+ __  __  __   __   __   ___       __   ___
+/  \  / /  \ |  \ |__) |__  |__/ |  \ |__  \  /
+\__/ /_ \__/ |__/ |__) |___ |  \ |__/ |___  \/
+
+                                                               >
+    Wordlist Generator | ozodbekdev.uz | tg: @ozodbekdevv
+"
+python3 wordlist.py
